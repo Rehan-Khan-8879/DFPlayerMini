7605c606017cf6d5973cc40cba92f2c461b45687
Rehan Khan <k.rehan8879@gmail.com>
2024-09-28 16:03:35 +0530

    DFPlayerMiniTest.ino

--- DFPlayerMiniTest.ino
+++ DFPlayerMiniTest.ino
@@ -1,10 +1,10 @@
-#include "Arduino.h"
-#include "DFRobotDFPlayerMini.h"
-#include <SoftwareSerial.h>
+#include "Arduino.h"    // Libarary 
+#include "DFRobotDFPlayerMini.h"  // libarary
+#include <SoftwareSerial.h>    // libarary
 
-#define button 4
-
-int buttonval =1 ;
+#define button 4   
+// defining the arduino pin for pushbutton
+int buttonval =1 ;   // Variable To Store pushbutton value
 
 // Initialize SoftwareSerial on pins 2 (RX) and 3 (TX)
 SoftwareSerial mySerial(2, 3);
@@ -13,12 +13,12 @@ DFRobotDFPlayerMini myDFPlayer;
 void setup() {
   // Begin serial communication with DFPlayer Mini
 
-  mySerial.begin(9600);
-  Serial.begin(115200);
+  mySerial.begin(9600);  
+  Serial.begin(115200);  // for Serial communication
   myDFPlayer.begin(mySerial);
-  pinMode(button,INPUT_PULLUP);
+  pinMode(button,INPUT_PULLUP); // setting pin as pullup
 
-  // Initialize DFPlayer Mini with software serial
+  
  
 
   // Set volume and play the first MP3 file
@@ -29,12 +29,13 @@ void setup() {
 
 void loop() {
   // Empty loop, no further actions required
-  buttonval = digitalRead(button);
-  if(buttonval == 0){
-    myDFPlayer.next();
-    delay(500);
+  buttonval = digitalRead(button);   //reading the value of button
+  
+  if(buttonval == 0){     
+    myDFPlayer.next();     // giving the condition for every buttonpress change the sound track and play next sound
+    delay(500);             //  giving delay for releasing pushbutton
   }
-  Serial.println(buttonval);
+  Serial.println(buttonval);     // for testing pourpose 
 
 
  // myDFPlayer.play(1);
